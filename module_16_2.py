26530779d0f5ad3a31b0120950fb631b8834aa14
Александр <alexco@list.ru>
2024-12-26 22:32:13 +0300

    Add files via upload
    Домашнее задание по теме "Валидация данных".

---
+++ module_16_2.py
@@ -0,0 +1,46 @@
+# Домашнее задание по теме "Валидация данных".
+
+# импорт библиотеки FastAPI
+from fastapi import FastAPI, Path
+from typing import Annotated
+
+# Создано приложение(объект) FastAPI
+app = FastAPI()
+
+
+# Создан декоратор маршрута к главной странице
+@app.get("/")
+async def get_p_main() -> dict:
+    return {'message': 'Главная страница'}
+
+
+# Создан декоратор маршрута к странице администратора
+@app.get("/user/admin")
+async def get_p_admin() -> dict:
+    return {'message': 'Вы вошли как администратор'}
+
+
+# Создан декоратор маршрута к странице пользователя c валидациeй аргумента user_id
+@app.get("/user/{user_id}")
+async def get_p_user_id(
+        user_id:Annotated[int, Path(ge=1,
+                                    le=100,
+                                    description= 'Введите свой ID',
+                                    example='17')]
+) -> dict:
+    return {'message': f'Вы вошли как пользователь №{user_id}'}
+
+
+# Создан декоратор маршрута к страницам пользователей c валидациeй аргументов username и age
+@app.get("/user/{username}/{age}")
+async def get_p_data_user(
+        username:Annotated[str, Path(min_length=5,
+                                     max_length=20,
+                                     description='Введите свое имя',
+                                     example='Vasiliy')],
+        age:Annotated[int, Path(ge=18,
+                                le=120,
+                                description='Введите свой возраст',
+                                example='21')]
+) -> dict:
+    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
